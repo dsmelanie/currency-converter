a64b7cd2b8d46d4505e5f3e0c8f48474d6a3a1c5
Mélanie <dsmelanie@outlook.fr>
2023-05-01 13:21:20 +0200

    Installation du React model

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import logo from 'src/assets/logo.png';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Currency Converter</h1>
+      <img width="50px" src={logo} alt="Logo" />
+    </div>
+  );
+}
+
+export default App;
